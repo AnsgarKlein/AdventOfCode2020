519946471697755d29054e77bf0a79bd8897ad21
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-12-21 22:10:57 +0100

    Day 17: Part Two

--- day17.py
+++ day17.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import Dict
 from typing import List
 from typing import Tuple
 
@@ -7,8 +8,11 @@ import sys
 from collections import namedtuple
 
 Cube = namedtuple('Cube', 'x y z')
+HyperCube = namedtuple('HyperCube', 'x y z w')
 
 def print_cubes(cubes: List[Cube]):
+    assert len(cubes[0]) == 3
+
     max_x, max_y, max_z = max_dimensions(cubes)
     min_x, min_y, min_z = min_dimensions(cubes)
 
@@ -39,6 +43,8 @@ def max_dimensions(cubes: List[Cube]) -> Tuple[int, int, int]:
     return max_x, max_y, max_z
 
 def neighbor_cubes(cube: Cube) -> List[Cube]:
+    assert len(cube) == 3
+
     others = []
     for z in [ cube.z, cube.z - 1, cube.z + 1 ]:
         for y in [ cube.y, cube.y - 1, cube.y + 1 ]:
@@ -49,6 +55,23 @@ def neighbor_cubes(cube: Cube) -> List[Cube]:
     others.remove(cube)
 
     assert len(others) == 26
+
+    return others
+
+def neighbor_hypercubes(cube: HyperCube) -> List[HyperCube]:
+    assert len(cube) == 4
+
+    others = []
+    for w in [ cube.w, cube.w - 1, cube.w + 1 ]:
+        for z in [ cube.z, cube.z - 1, cube.z + 1 ]:
+            for y in [ cube.y, cube.y - 1, cube.y + 1 ]:
+                for x in [ cube.x, cube.x - 1, cube.x + 1 ]:
+                    others.append(HyperCube(x, y, z, w))
+
+    # Cube is not a neighbor of itself
+    others.remove(cube)
+
+    assert len(others) == 80
 
     return others
 
@@ -74,7 +97,9 @@ def read_input_file(filename) -> List[Cube]:
 
     return cubes
 
-def cycle(cubes: List[Cube]) -> List[Cube]:
+def cycle_cubes(cubes: List[Cube]) -> List[Cube]:
+    assert len(cubes[0]) == 3
+
     max_x, max_y, max_z = max_dimensions(cubes)
     max_x += 1
     max_y += 1
@@ -113,6 +138,32 @@ def cycle(cubes: List[Cube]) -> List[Cube]:
 
     return new_cubes
 
+def cycle_hypercubes(cubes: List[HyperCube]) -> List[HyperCube]:
+    assert len(cubes[0]) == 4
+
+    # Add cubes after cycle to new list
+    new_cubes: List[HyperCube] = []
+
+    # Map all neighbors (those that might become active)
+    # of active nodes to their active neighbor count
+    cube_neighbor: Dict[HyperCube, int] = dict()
+    for current in cubes:
+        for neighbor in neighbor_hypercubes(current):
+            if neighbor not in cube_neighbor:
+                cube_neighbor[neighbor] = 1
+            else:
+                cube_neighbor[neighbor] += 1
+
+    # Cubes with 3 neighbors become active
+    # Cubes with 2 neighbors only stay active
+    for current, count in cube_neighbor.items():
+        if count == 3:
+            new_cubes.append(current)
+        elif count == 2 and current in cubes:
+            new_cubes.append(current)
+
+    return new_cubes
+
 def main():
     # Read input file
     cubes = read_input_file('day17_input.txt')
@@ -124,12 +175,30 @@ def main():
     new_cubes = cubes
     print('Booting up experimental pocket dimension ', end = '')
     for _ in range(6):
-        new_cubes = cycle(new_cubes)
+        new_cubes = cycle_cubes(new_cubes)
         print('.', end = '')
         sys.stdout.flush()
     print('')
 
     print('Cubes in active state after bootup: {}'.format(len(new_cubes)))
+
+
+    ############ PART TWO ############
+
+    # Convert cubes to hypercubes
+    print('Converting 3 dimensions to Hypercubes ...')
+    hypercubes = [ HyperCube(cube.x, cube.y, cube.z, 0) for cube in cubes ]
+
+    # Cycle six times
+    new_hypercubes = hypercubes
+    print('Booting up experimental 4D pocket dimension ', end = '')
+    for _ in range(6):
+        new_hypercubes = cycle_hypercubes(new_hypercubes)
+        print('.', end = '')
+        sys.stdout.flush()
+    print('')
+
+    print('Hypercubes in active state after bootup: {}'.format(len(new_hypercubes)))
 
 if __name__ == '__main__':
     main()
