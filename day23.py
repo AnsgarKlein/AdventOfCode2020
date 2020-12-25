4989940274d0909d61c4a6ea89aa237c0ef3ea7b
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-12-25 16:13:28 +0100

    Day 23: Part Two

--- day23.py
+++ day23.py
@@ -2,20 +2,29 @@
 
 from typing import List
 
+import sys
+
 def read_input_file(filename):
     with open(filename, 'r') as input_file:
         return [int(num) for num in input_file.read() if num != '\n']
 
-def moves(cups: List[int], num_moves: int):
+def moves(cups: List[int], num_moves: int, verbose = True):
     move_id = 1
     current_id = 0
     len_cups = len(cups)
 
+    if not verbose:
+        print('Move: 0', end = '')
+
     for _ in range(num_moves):
         current = cups[current_id]
 
-        print(f'-- move {move_id} --')
-        print('cups: {}'.format(' '.join(['({})'.format(c) if c == current else str(c) for c in cups])))
+        if verbose:
+            print(f'-- move {move_id} --')
+            print('cups: {}'.format(' '.join(['({})'.format(c) if c == current else str(c) for c in cups])))
+        else:
+            print(f'\rMove: {move_id}', end = '')
+            sys.stdout.flush()
 
         # Determine index of 3 cups after current cup
         removed_ids = [(current_id + i) % len_cups for i in range(1,4)]
@@ -29,14 +38,16 @@ def moves(cups: List[int], num_moves: int):
             if removed_id < current_id:
                 current_id -= 1
 
-        print('pick up: {}'.format(', '.join([str(i) for i in removed])))
+        if verbose:
+            print('pick up: {}'.format(', '.join([str(i) for i in removed])))
 
         # Select destination cup
         destination = current - 1
         while destination < 1 or destination > len_cups or destination in removed:
             destination = (destination - 1) % (len_cups + 1)
 
-        print('destination: {}\n'.format(destination))
+        if verbose:
+            print('destination: {}\n'.format(destination))
 
         # Put 3 picked up cups behind destination cup
         destination_id = cups.index(destination) + 1
@@ -48,8 +59,11 @@ def moves(cups: List[int], num_moves: int):
         current_id = (current_id + 1) % len_cups
         move_id += 1
 
-    print('-- final --')
-    print('cups: {}\n'.format(' '.join(['({})'.format(c) if c == current else str(c) for c in cups])))
+    if verbose:
+        print('-- final --')
+        print('cups: {}\n'.format(' '.join(['({})'.format(c) if c == current else str(c) for c in cups])))
+    else:
+        print()
 
 def main():
     ############ PART ONE ############
@@ -65,6 +79,26 @@ def main():
     while checksum[0] != 1:
         checksum.append(checksum.pop(0))
     print('Checksum: {}\n'.format(''.join((str(e) for e in checksum if e != 1))))
+
+
+    ############ PART TWO ############
+
+    # Read input again
+    cups = read_input_file('day23_input.txt')
+
+    # Extend cups to one million cups
+    cups.extend(list(range(max(cups) + 1, 1_000_000 + 1)))
+    assert len(cups) == 1_000_000
+
+    # Play ten million moves
+    # This works but depending on CPU takes more
+    # than 20 hours to finish.
+    moves(cups, 10_000_000, False)
+
+    # Print checksum of two cups after cup 1
+    c1 = cups[(cups.index(1)  + 1) % len(cups)]
+    c2 = cups[(cups.index(c1) + 1) % len(cups)]
+    print('Checksum: {} ({}, {})'.format(c1 * c2, c1, c2))
 
 if __name__ == '__main__':
     main()
